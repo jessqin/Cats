008590829adae87cdcc4f02ca1ccceaed7a422bd
Jessica Qin <jessicaq319@gmail.com>
2020-05-20 00:52:35 -0400

    Update __init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,6 +4,7 @@ from flask_mongoengine import MongoEngine
 from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 from flask_bcrypt import Bcrypt
 from werkzeug.utils import secure_filename
+from flask_mail import Mail
 
 # stdlib
 import os
@@ -12,16 +13,30 @@ from datetime import datetime
 # local
 from .client import CatClient
 
+import os
+
 app = Flask(__name__)
 # app.config["MONGO_URI"] = "mongodb://localhost:27017/second_database"
 app.config['MONGODB_HOST'] = 'mongodb://localhost:27017/final'
-app.config['SECRET_KEY'] = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
+#app.config['SECRET_KEY'] = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
+app.config['SECRET_KEY'] = os.urandom(16)
 
 # mongo = PyMongo(app)
 db = MongoEngine(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 bcrypt = Bcrypt(app)
+
+app.config.update(dict(
+    DEBUG = True,
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 587,
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = 'catwiki388j@gmail.com',
+    MAIL_PASSWORD = 'cmsc388j',
+))
+mail = Mail(app)
 
 client = CatClient(os.environ.get('CAT_API_KEY'))
 
