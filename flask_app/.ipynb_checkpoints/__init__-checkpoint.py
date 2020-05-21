c6927755e482de2f6c034665a6e900aeba6a5313
Jessica Q <jessicaq319@gmail.com>
2020-05-21 00:08:03 -0400

    changed uri

--- flask_app/.ipynb_checkpoints/__init__-checkpoint.py
+++ flask_app/.ipynb_checkpoints/__init__-checkpoint.py
@@ -4,6 +4,11 @@ from flask_mongoengine import MongoEngine
 from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 from flask_bcrypt import Bcrypt
 from werkzeug.utils import secure_filename
+from flask_mail import Mail
+from flask_talisman import Talisman
+
+app = Flask(__name__)
+Talisman(app)
 
 # stdlib
 import os
@@ -12,10 +17,38 @@ from datetime import datetime
 # local
 from .client import CatClient
 
+import os
+
 app = Flask(__name__)
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/second_database"
-app.config['MONGODB_HOST'] = 'mongodb://localhost:27017/final'
-app.config['SECRET_KEY'] = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
+#app.config["MONGO_URI"] = "mongodb://heroku_62h2lcbv:hq81vpbtmn5lvg42nu2mbnlcvo@ds163020.mlab.com:63020/heroku_62h2lcbv"
+app.config['MONGODB_HOST'] = 'mongodb://heroku_62h2lcbv:hq81vpbtmn5lvg42nu2mbnlcvo@ds163020.mlab.com:63020/heroku_62h2lcbv'
+#app.config['SECRET_KEY'] = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
+app.config['SECRET_KEY'] = os.urandom(16)
+
+csp = {
+    'default-src': '*',
+    'img-src': 
+        ['data:', 
+        '*'
+        ],
+    'style-src':
+        ['\'unsafe-inline\' \'self\'',
+        'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'
+        ],
+    'script-src': 
+        ['\'unsafe-inline\' \'self\'',
+            'https://code.jquery.com/jquery-3.4.1.slim.min.js', 
+            'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
+            'https://code.jquery.com/jquery-3.4.1.slim.min.js',
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js'        
+        ],
+    "content_security_policy": "style-src 'unsafe-inline'" 
+}
+
+Talisman(app, 
+    content_security_policy=csp,
+    content_security_policy_report_uri='/csp_reports' 
+)
 
 # mongo = PyMongo(app)
 db = MongoEngine(app)
@@ -23,6 +56,21 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 bcrypt = Bcrypt(app)
 
-client = CatClient(os.environ.get('CAT_API_KEY'))
+app.config.update(dict(
+    DEBUG = True,
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 587,
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = 'catwiki388j@gmail.com',
+    MAIL_PASSWORD = 'cmsc388j',
+))
+mail = Mail(app)
 
-from . import routes
+from flask_app.features.routes import features
+from flask_app.users.routes import users
+from flask_app.description.routes import description
+
+app.register_blueprint(users)
+app.register_blueprint(features)
+app.register_blueprint(description)
